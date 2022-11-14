a48c82b58676d0d9c0d0113c560ef3cf2c27123b
adam.pilgermayer <adam0002neet@gmail.com>
2022-11-14 15:20:51 +0100

    update_2

--- js/nav.js
+++ js/nav.js
@@ -1,13 +1,15 @@
 const burger = document.querySelector(".burger");
 const menu = document.querySelector(".navbar-menu");
-const menuItems = document.querySelectorAll(".navbar.menu__link");
+const menuItems = [...document.querySelectorAll(".navbar-menu__link")];
 
 burger.addEventListener("click", () => {
 	burger.classList.toggle("on");
 	menu.classList.toggle("toggled");
 });
 
-[...menuItems].addEventListener("click", () => {
-	burger.classList.toggle("on");
-	menu.classList.toggle("toggled");
+menuItems.forEach((current) => {
+	current.addEventListener("click", () => {
+		burger.classList.toggle("on");
+		menu.classList.toggle("toggled");
+	});
 });
