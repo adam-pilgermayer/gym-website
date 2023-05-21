52ae7c30d2f81e0ce85638afe5b0bea36ac593c2
adam.pilgermayer <adam0002neet@gmail.com>
2023-05-21 14:53:42 +0200

    update all

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 const $navbar = document.querySelector(".navbar");
+const $burger = document.querySelector(".js-hamburger");
+const $mobileMenu = document.querySelector(".navbar__mobile-menu");
 
 const toggleNavbar = () => {
-	$navbar.classList.toggle("nav-scrolled", window.scrollY > 0);
+	$navbar.classList.toggle("nav-scrolled", window.scrollY > 25);
 };
+
+
+const toggleBurgerMenu = () => {
+	$burger.classList.toggle("open-burger");
+}
+
+
+
 window.addEventListener("scroll", toggleNavbar);
+
+
+$burger.addEventListener("click", toggleBurgerMenu);
